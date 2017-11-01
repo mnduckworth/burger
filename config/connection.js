10038b5627918d69bec14f5f0fd672366e629c10
Matthew Duckworth <matthewduckworth100@gmail.com>
2017-11-01 18:15:32 -0500

    added jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,19 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "pieeater",
-  database: "burgers_db"
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+  connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "pieeater",
+    database: "burgers_db"
+  });
+}
 
 connection.connect(function(err) {
   if (err) {
